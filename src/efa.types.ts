97fe65c9a3702559807d0b34b88601885bcf4e0a
Kenneth Tsang <me@jxeeno.com>
2023-10-12 17:11:39 +1100

    chore(): use spaces

--- src/efa.types.ts
+++ src/efa.types.ts
@@ -1,40 +1,40 @@
 export type EfaRapidJsonStopPartial = {
-	id: string,
-	parent?: EfaRapidJsonStopPartial,
-	type: string,
-	coord: [number, number]
-	isGlobalId: boolean;
+    id: string,
+    parent?: EfaRapidJsonStopPartial,
+    type: string,
+    coord: [number, number]
+    isGlobalId: boolean;
     departureTimePlanned: string,
-	departureTimeEstimated?: string,
-	arrivalTimePlanned: string,
-	arrivalTimeEstimated?: string,
+    departureTimeEstimated?: string,
+    arrivalTimePlanned: string,
+    arrivalTimeEstimated?: string,
 }
 
 export type EfaRapidJsonLegPartial = {
-	origin: EfaRapidJsonStopPartial,
-	destination: EfaRapidJsonStopPartial,
-	transportation: {
-		id: string,
-		number: string,
-		disassembledName: string,
-		iconId: number,
-		product: {
-			class: number,
-			name: string,
-			iconId: number
-		},
-		operator: {
-			id: string,
-			name: string
-		},
-	}
+    origin: EfaRapidJsonStopPartial,
+    destination: EfaRapidJsonStopPartial,
+    transportation: {
+        id: string,
+        number: string,
+        disassembledName: string,
+        iconId: number,
+        product: {
+            class: number,
+            name: string,
+            iconId: number
+        },
+        operator: {
+            id: string,
+            name: string
+        },
+    }
 }
 
 export type EfaRapidJsonTripPartial = {
-	journeys: Array<{
-		legs: EfaRapidJsonLegPartial[],
+    journeys: Array<{
+        legs: EfaRapidJsonLegPartial[],
         fare?: {
             tickets?: any[]
         }
-	}>
+    }>
 }
