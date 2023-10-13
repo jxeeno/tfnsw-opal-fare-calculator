82dc23610c0cd9f4cec5c6dfa84dea71ef8a9bc4
Kenneth Tsang <me@jxeeno.com>
2023-10-13 12:28:38 +1100

    feat(): add specific case for trackwork bus

--- download-test-snapshots/download.js
+++ download-test-snapshots/download.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const API_KEY = process.env.API_KEY;
 
+const SPECIFIC_COMBINATIONS = [
+    ["213510", "213110", 'TRACKWORK_BUS_NO_OPAL', {itdDate: "20231013", itdTime: "2205"}]
+]
+
 // a list of TSNs to generate snapshots against
 const CANDIDATE_TSNS = [
     // Ferry wharfs
@@ -44,42 +48,50 @@ const CANDIDATE_TSNS = [
 
 // a list of date times to test against
 const TIMES = {
-    WEEKDAY_METRO_PRE_AM_PEAK: {itdTime: '0500', itdDate: '20231012'},
-    WEEKDAY_OUTER_METRO_PRE_AM_PEAK: {itdTime: '0630', itdDate: '20231012'},
+    NEAR_END_OF_OPAL_DAY: {itdTime: '0330', itdDate: '20231013'},
+    // WEEKDAY_METRO_PRE_AM_PEAK: {itdTime: '0500', itdDate: '20231012'},
+    // WEEKDAY_OUTER_METRO_PRE_AM_PEAK: {itdTime: '0630', itdDate: '20231012'},
 };
 
 async function main() {
+    const queryAndSave = async (origin, destination, timeKey, timeParams) => {
+        const combinationKey = `${origin}_${destination}_${timeKey}`;
+        const pathName = `../src/tests/snapshots/${combinationKey}.json`;
+        if(!fs.existsSync(pathName)){
+            const {data} = await axios.get('https://api.transport.nsw.gov.au/v1/tp/trip', {
+                params: {
+                    outputFormat: "rapidJSON",
+                    coordOutputFormat: "EPSG:4326",
+                    depArrMacro: "dep",
+                    type_origin: "any",
+                    name_origin: origin,
+                    type_destination: "any",
+                    name_destination: destination,
+                    calcNumberOfTrips: "6",
+                    TfNSWTR: "true",
+                    ...timeParams
+                },
+                headers: {
+                    Authorization: `apikey ${API_KEY}`
+                }
+            });
+
+            fs.writeFileSync(pathName, JSON.stringify(data));
+            console.log(pathName);
+        }
+    }
     // const combinations = [];
+    for(const [origin, destination, timeKey, timeParams] of SPECIFIC_COMBINATIONS){
+        await queryAndSave(origin, destination, timeKey, timeParams)
+    }
     for(const origin of CANDIDATE_TSNS){
         for(const destination of CANDIDATE_TSNS){
             if(origin !== destination){
                 // combinations.push(`${origin}->${destination}`);
                 for(const timeKey in TIMES){
                     const timeParams = TIMES[timeKey];
-                    const combinationKey = `${origin}_${destination}_${timeKey}`;
-                    const pathName = `../src/tests/snapshots/${combinationKey}.json`;
-                    if(!fs.existsSync(pathName)){
-                        const {data} = await axios.get('https://api.transport.nsw.gov.au/v1/tp/trip', {
-                            params: {
-                                outputFormat: "rapidJSON",
-                                coordOutputFormat: "EPSG:4326",
-                                depArrMacro: "dep",
-                                type_origin: "any",
-                                name_origin: origin,
-                                type_destination: "any",
-                                name_destination: destination,
-                                calcNumberOfTrips: "6",
-                                TfNSWTR: "true",
-                                ...timeParams
-                            },
-                            headers: {
-                                Authorization: `apikey ${API_KEY}`
-                            }
-                        });
-
-                        fs.writeFileSync(pathName, JSON.stringify(data));
-                        console.log(pathName);
-                    }
+                    
+                    await queryAndSave(origin, destination, timeKey, timeParams)
                 }
             }
         }    
